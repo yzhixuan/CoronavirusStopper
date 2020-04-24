f3d95845854f69a5b60a52e1a8ad54a86ddf90c5
zjh <zjh@umich.edu>
2020-04-24 19:13:31 -0400

    Combined Calculations

--- Calculation.py
+++ Calculation.py
@@ -8,6 +8,41 @@ f = open("CalculationOutput.txt", "w")
 cur.execute("SELECT * FROM TotalByCountry")
 f.write("Total Number of Country: %d \n" % len(list(cur)))
 
+#Chenyiyang
+cur.execute('SELECT state, positive, recovered, death From USstatescurrent')
+data=[]
+for row in cur:
+    data.append(row)
+#print(data)
+totalpositivecases=0
+totaldeath=0
+totalrecovered=0
+for row in data:
+    if row[1]!=None:
+        totalpositivecases+=row[1]
+    if row[3]!=None:
+        totaldeath+=row[3]
+    if row[2]!=None:
+        totalrecovered+=row[2]
+f.write("State distribution of positive cases:\n")
+for row in data:
+        f.write("State {} contains {} percent of total US positive cases.\n".format(row[0],100*row[1]/totalpositivecases))
+f.write("US and each states death rate:\n")
+f.write("US death rate of coronavirus is {} precent.\n".format(100*totaldeath/totalpositivecases))
+for row in data:
+    if row[3]!=None or 0:
+        f.write("State {} death rate is {}\n".format(row[0],100*row[3]/row[1]))
+    else:
+        f.write("State {} death rate is unavailable".format(row[0]))
+f.write("US and each states recover rate:\n")
+f.write("US recover rate of coronavirus is {} percent.\n".format(100*totalrecovered/totalpositivecases))
+for row in data:
+    if row[2]!=None:
+        f.write("State {} recover rate is {} percent.\n".format(row[0],100*row[2]/row[1]))
+    else:
+        f.write("State {} recover rate unavailable.\n".format(row[0]))
+
+
 cur.execute("SELECT * FROM TotalByCountry WHERE TotalConfirmed > 10000")
 f.write("Country with more than 10000 confirmed cases: %d \n" % len(list(cur)))
 cur.execute("SELECT * FROM TotalByCountry WHERE TotalConfirmed > 5000")
